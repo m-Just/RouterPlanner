cdb232672911507d14b0e37a772b5a4671aaf8d2
m-Just <mjust.lkc@gmail.com>
2016-04-09 14:24:18 +0800

    new commit

--- future_net/route.cpp
+++ future_net/route.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 struct sp {
+    int id;
     int sNode;
     int eNode;
     int weight;
@@ -13,6 +14,21 @@ struct sp {
 //你要完成的功能总入口
 void search_route(char *topo[5000], int edge_num, char *demand)
 {
+    subpath* sp = (subpath*)malloc(sizeof(subpath) * edge_num);
+    
+    int i;
+    char *ptr;
+    for (i = 0; i < edge_num; i++) {
+        scan(topo[i]);
+        sp[i].id = num[0];
+        sp[i].sNode = num[1];
+        sp[i].eNode = num[2];
+        sp[i].weight = num[3];
+    }
+
+    
+
+
 /*
     unsigned short result[] = {2, 6, 3};//示例中的一个解
 
